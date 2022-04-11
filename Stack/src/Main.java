5f6ca06511bf25157348d34afb44db7eeb8b1c0d
jaehyukjung <jhdool1594@gmail.com>
2022-04-11 14:54:57 +0900

    [04.08] 자료구조및알고리즘1 스택 구현

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,13 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] argv) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        stack st = new stack();
+        Scanner sc = new Scanner(System.in);
 
+        stack st = new stack();
         String str = br.readLine();
         for(int i=0;i<str.length();i++){
             char ch = str.charAt(i);
@@ -51,6 +54,44 @@ public class Main {
         int N = st.size();
         for(int i=0;i<N+1;i++){
             System.out.print(st.pop());
+        }
+
+
+
+        //실습과제 2
+        st = new stack();
+        String ch = "1";
+        while(ch !="0"){
+            ch = sc.next();
+            if(ch.equals("/")){
+                int num1 = Integer.valueOf(Objects.toString(st.pop())) ;
+                int num2 = Integer.valueOf(Objects.toString(st.pop())) ;
+                st.push(num2/num1);
+            }
+            else if(ch.equals("*")){
+                int num1 = Integer.valueOf(Objects.toString(st.pop())) ;
+                int num2 = Integer.valueOf(Objects.toString(st.pop())) ;
+                st.push(num2*num1);
+            }
+            else if(ch.equals("+")){
+                int num1 = Integer.valueOf(Objects.toString(st.pop())) ;
+                int num2 = Integer.valueOf(Objects.toString(st.pop())) ;
+                st.push(num2+num1);
+            }
+            else if(ch.equals("-")){
+                int num1 = Integer.valueOf(Objects.toString(st.pop())) ;
+                int num2 = Integer.valueOf(Objects.toString(st.pop())) ;
+                st.push(num2-num1);
+            }
+            else {
+                st.push(ch);
+            }
+            if(ch.equals("0")) {
+                System.out.println(st.pop());
+                break;
+            }
+
+            st.showStack();
         }
 
     }
