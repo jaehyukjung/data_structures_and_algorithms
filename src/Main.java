9251cb2eec8be6f8b6b5b69bd45cb97acf404cdc
jaehyukjung <jhdool1594@gmail.com>
2022-05-30 17:12:29 +0900

    [05.30] 자료구조및알고리즘1 탐색

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,12 @@ import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Main {
-    static int[] arr;
-
-    public static void seqSearch2(int key, int high){
-        int i;
-
-        arr[high+1] = key;
-        for(i = 0; arr[i] != key ; i++) ;
-
-        if(i == (high + 1)) System.out.println("실패");
-        else System.out.println("성공 : "+(i+1)+"번째에 존재.");
-    }
 
     public static void main(String[] argv) throws IOException{
-        Random rand = new Random();
+        Random rand = new Random();  // 랜덤 인스턴스 생성
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        arr = new int[rand.nextInt(100)+1];
+
+        int[] arr = new int[rand.nextInt(100)+1]; //크기가 1에서 100 사이인 변수 선언
         System.out.println(arr.length);
         for (int i=0;i<arr.length-1;i++){
             arr[i] = rand.nextInt()+1;
@@ -29,9 +19,10 @@ public class Main {
         }
         System.out.println();
 
+
         int key = Integer.parseInt(br.readLine()); //찾고자 하는 값.
         arr[arr.length-1] = key;
-        seqSearch2(key,arr.length-2);
+        seqSearch(arr,key,arr.length-2); //인덱스는 0부터 시작하고 배열의 끝에는 target값이 들어있으므로 배열크기 -2만큼 설정합니다.
 
 
 
@@ -40,31 +31,47 @@ public class Main {
 
         sort(arr2,0,arr2.length-1);
         printdata(arr2);
-        binsearch(arr2,36);
+        System.out.println("찾고자 하는 값을 입력하세요 : ");
+        int num =Integer.parseInt(br.readLine());  //찾고자 하는 값을 입력해서 비교
+        binsearch(arr2,num);
     }
 
 
-    public static int binsearch(int arr[],int target){
+    public static void seqSearch(int[] arr, int target, int high){  //과제1
+        int i;
+
+        arr[high+1] = target;  //배열 끝값에 target 값을 집어 넣어 for문 조건에서 받드시 종료되도록 설정
+        for(i = 0; arr[i] != target ; i++) ; //이렇게 하면 기존 배열에 찾고자 하는 값이 없어도 마지막에 target을 추가해줬기때문에 배열 크기까지 간다면 반드시 종료된다.
+
+        if(i == (high + 1)) System.out.println("실패");  //만약 high 즉 배열의 크기와 같은 값이 출력되면 찾고자 하는 값이 없으프로 실패를 출력
+        //이를 위해서 i를 초기에 설정해 for문이 끝나도 값이 유지되도록 설정. 또한 i++의 조건이 있으므로 high까지 돌아도 high +1이 i에 저장되기 때문에 이렇게 설정했습니다.
+        else System.out.println("성공 : "+(i+1)+"번째에 존재.");
+    }
+
+
+    public static int binsearch(int arr[],int target){  //과제 2 이진탐색 구현
         int start =0;
         int end = arr.length-1;
-        int mid =(start+end)/2;
+        int mid =(start+end)/2;  //인덱스 중간 값을 기준으로 비교 후
         while(end-start>= 0){
-            if(arr[mid]== target){
+            if(arr[mid]== target){  //중간값이 찾고자 하는 값과 같다면 성공을 출력하고 mid 즉 target의 인덱스 번호를 반환하도록 한다.
                 System.out.println("탐색성공 : "+mid+"번째 인덱스");
                 return mid;
             }
-            else if(arr[mid]<= target){
-                start= mid+1;
+            else if(arr[mid]<= target){ //타겟보다 값이 작다면
+                start= mid+1;           //정렬된 배열의 중간보다 우측에 있으니 끝값은 그대로 두고, 시작값을 중간값보다 1큰값으로 설정한다. (인덱스기준)
             }
-            else{
-                end= mid-1;
+            else{  //타겟보다 큰경우면,
+                end= mid-1;  // 정렬된 배열의 중간보다 좌측에 있으니 시작값은 그대로 두고, 끝값을 중간값보다 1 작은값으로 설정한다. (인덱스기준)
             }
-            System.out.println("현재 탐색 비교 값: "+arr[mid]);
-            mid= (end+start)/2;
+            System.out.println("현재 탐색 비교 값: "+arr[mid]); //arr[mid]값과 비교하고 어떤 값과 비교했는지 보여주는 문장입니다.
+            mid= (end+start)/2; // 그 후 mid값을 분기문을 통해 수정된 start와 end의 중간값으로 다시 실행한다.
         }
         System.out.println("탐색실패");
         return 0;
     }
+
+
 
     //과거 사용한 퀵정렬 활용.
     public static void sort(int[] a){
@@ -99,7 +106,7 @@ public class Main {
         a[low]=a[high];
         a[high]=temp;
     }
-    private static void printdata(int arr[]){
+    private static void printdata(int arr[]){  //기존에 만들어둔 배열 내부를 보여주는 함수입니다.
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
